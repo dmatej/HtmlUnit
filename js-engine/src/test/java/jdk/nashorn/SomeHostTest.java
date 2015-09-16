64f0e5d77b1aef88319f5c2b4219ffdd8596c60b
Ahmed Ashour <asashour@users.noreply.github.com>
2015-09-16 06:49:44 +0000

    [JS] checkstyle

--- js-engine/src/test/java/jdk/nashorn/SomeHostTest.java
+++ js-engine/src/test/java/jdk/nashorn/SomeHostTest.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright (c) 2015 Gargoyle Software Inc.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (http://www.gnu.org/licenses/).
+ */
 package jdk.nashorn;
 
 import javax.script.ScriptEngine;
