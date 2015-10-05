7646b2bbb90157397a6bdf3f77686f320a127496
Ahmed Ashour <asashour@users.noreply.github.com>
2015-10-05 06:55:00 +0000

    [JS] no need to detect the ScriptObject type

--- js/src/main/java/com/gargoylesoftware/js/nashorn/ScriptUtils.java
+++ js/src/main/java/com/gargoylesoftware/js/nashorn/ScriptUtils.java
@@ -23,7 +23,6 @@ import java.util.Map;
 
 import com.gargoylesoftware.js.nashorn.internal.objects.annotations.Browser;
 import com.gargoylesoftware.js.nashorn.internal.objects.annotations.BrowserFamily;
-import com.gargoylesoftware.js.nashorn.internal.objects.annotations.Constructor;
 import com.gargoylesoftware.js.nashorn.internal.objects.annotations.Function;
 import com.gargoylesoftware.js.nashorn.internal.objects.annotations.Getter;
 import com.gargoylesoftware.js.nashorn.internal.objects.annotations.Setter;
@@ -31,7 +30,6 @@ import com.gargoylesoftware.js.nashorn.internal.objects.annotations.WebBrowser;
 import com.gargoylesoftware.js.nashorn.internal.runtime.AccessorProperty;
 import com.gargoylesoftware.js.nashorn.internal.runtime.Property;
 import com.gargoylesoftware.js.nashorn.internal.runtime.PropertyMap;
-import com.gargoylesoftware.js.nashorn.internal.runtime.PrototypeObject;
 import com.gargoylesoftware.js.nashorn.internal.runtime.ScriptFunction;
 import com.gargoylesoftware.js.nashorn.internal.runtime.ScriptObject;
 
@@ -41,75 +39,63 @@ public class ScriptUtils {
         final BrowserFamily browserFamily = Browser.getCurrent().getFamily();
         final int browserVersion = Browser.getCurrent().getVersion();
         final Class<?> enclosingClass = scriptObject.getClass().getEnclosingClass();
-        boolean isConstructor = false;
-        for (final Method m : enclosingClass.getDeclaredMethods()) {
-            for (final Constructor constructor : m.getAnnotationsByType(Constructor.class)) {
-                if (isSupported(constructor.browsers(), browserFamily, browserVersion)) {
-                    isConstructor = true;
+        final List<Property> list = new ArrayList<>(2);
+
+        final Method[] allMethods = enclosingClass.getDeclaredMethods();
+        final Map<String, Method> setters = new HashMap<>();
+        final MethodHandles.Lookup lookup = MethodHandles.lookup();
+        for (final Method method : allMethods) {
+            for (final Function function : method.getAnnotationsByType(Function.class)) {
+                if (isSupported(function.browsers(), browserFamily, browserVersion)) {
+                    final String methodName = method.getName();
+                    list.add(AccessorProperty.create(methodName, function.attributes(), 
+                            virtualHandle(scriptObject.getClass(), "G$" + methodName,
+                                    ScriptFunction.class),
+                            virtualHandle(scriptObject.getClass(), "S$" + methodName,
+                                    void.class, ScriptFunction.class)));
+                    try {
+                        final ScriptFunction scriptFunction =
+                                ScriptFunction.createBuiltin(method.getName(), lookup.unreflect(method));
+                        final MethodHandle handle =
+                                lookup.findSetter(scriptObject.getClass(), methodName, ScriptFunction.class);
+                        handle.invoke(scriptObject, scriptFunction);
+                    }
+                    catch(final Throwable t) {
+                        throw new RuntimeException(t);
+                    }
+                }
+            }
+            for (final Setter setter : method.getAnnotationsByType(Setter.class)) {
+                if (isSupported(setter.browsers(), browserFamily, browserVersion)) {
+                    String fieldName = method.getName().substring(3);
+                    fieldName = Character.toLowerCase(fieldName.charAt(0)) + fieldName.substring(1);
+                    setters.put(fieldName, method);
                 }
             }
         }
-        if ((isConstructor && PrototypeObject.class.isAssignableFrom(scriptObject.getClass()))
-            || (!isConstructor && !PrototypeObject.class.isAssignableFrom(scriptObject.getClass()))) {
+        for (final Method method : allMethods) {
+            for (final Getter getter : method.getAnnotationsByType(Getter.class)) {
+                if (isSupported(getter.browsers(), browserFamily, browserVersion)) {
+                    MethodHandle setter = null;
+                    String fieldName = method.getName().substring(3);
+                    fieldName = Character.toLowerCase(fieldName.charAt(0)) + fieldName.substring(1);
+                    Method setterMethod = setters.get(fieldName);
 
-            final List<Property> list = new ArrayList<>(2);
-
-            final Method[] allMethods = enclosingClass.getDeclaredMethods();
-            final Map<String, Method> setters = new HashMap<>();
-            final MethodHandles.Lookup lookup = MethodHandles.lookup();
-            for (final Method method : allMethods) {
-                for (final Function function : method.getAnnotationsByType(Function.class)) {
-                    if (isSupported(function.browsers(), browserFamily, browserVersion)) {
-                        final String methodName = method.getName();
-                        list.add(AccessorProperty.create(methodName, function.attributes(), 
-                                virtualHandle(scriptObject.getClass(), "G$" + methodName,
-                                        ScriptFunction.class),
-                                virtualHandle(scriptObject.getClass(), "S$" + methodName,
-                                        void.class, ScriptFunction.class)));
-                        try {
-                            final ScriptFunction scriptFunction =
-                                    ScriptFunction.createBuiltin(method.getName(), lookup.unreflect(method));
-                            final MethodHandle handle =
-                                    lookup.findSetter(scriptObject.getClass(), methodName, ScriptFunction.class);
-                            handle.invoke(scriptObject, scriptFunction);
+                    try {
+                        if (setterMethod != null) {
+                            setter = lookup.unreflect(setterMethod);
                         }
-                        catch(final Throwable t) {
-                            throw new RuntimeException(t);
-                        }
+                        list.add(AccessorProperty.create(fieldName, getter.attributes(), 
+                                lookup.unreflect(method),
+                                setter));
                     }
-                }
-                for (final Setter setter : method.getAnnotationsByType(Setter.class)) {
-                    if (isSupported(setter.browsers(), browserFamily, browserVersion)) {
-                        String fieldName = method.getName().substring(3);
-                        fieldName = Character.toLowerCase(fieldName.charAt(0)) + fieldName.substring(1);
-                        setters.put(fieldName, method);
+                    catch (final IllegalAccessException e) {
+                        throw new RuntimeException(e);
                     }
                 }
             }
-            for (final Method method : allMethods) {
-                for (final Getter getter : method.getAnnotationsByType(Getter.class)) {
-                    if (isSupported(getter.browsers(), browserFamily, browserVersion)) {
-                        MethodHandle setter = null;
-                        String fieldName = method.getName().substring(3);
-                        fieldName = Character.toLowerCase(fieldName.charAt(0)) + fieldName.substring(1);
-                        Method setterMethod = setters.get(fieldName);
-
-                        try {
-                            if (setterMethod != null) {
-                                setter = lookup.unreflect(setterMethod);
-                            }
-                            list.add(AccessorProperty.create(fieldName, getter.attributes(), 
-                                    lookup.unreflect(method),
-                                    setter));
-                        }
-                        catch (final IllegalAccessException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                }
-            }
-            scriptObject.setMap(PropertyMap.newMap(list));
         }
+        scriptObject.setMap(PropertyMap.newMap(list));
     }
 
     private static MethodHandle virtualHandle(final Class<?> klass, final String name, final Class<?> rtype, final Class<?>... ptypes) {
